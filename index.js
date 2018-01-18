5810eff54fdebeac960c70c88a4bf86c75679bef
Laura Polanco <laurapolanco27@gmail.com>
2018-01-18 05:50:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
  if (string.toUpperCase() === string) {
-  return "YES INDEED!"
+  return ("YES INDEED!")
 }
   else if (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return ("I love you, too.")
   }
-  else 
+  else { 
+    return ("I can't hear you!")
 }
-
+}
 
  
   
